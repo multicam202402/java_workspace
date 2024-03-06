a0d6a743e2185e87dba90b03f4543119963769c3
multicampus202402 <multicam202402@gmail.com>
2024-03-06 10:59:36 +0900

    금일수업

--- project0305/src/com/sds/project0305/member/MemberJoin.java
+++ project0305/src/com/sds/project0305/member/MemberJoin.java
@@ -2,15 +2,21 @@ package com.sds.project0305.member;
 
 import java.awt.BorderLayout;
 import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
@@ -27,7 +33,7 @@ import javax.swing.JTextField;
  * 2) 해당 리스너의 메서드 오버라이드(개발자가 원하는 코드로 재정의)
  * 3) 컴포넌트와 리스너 연결
  * */
-public class MemberJoin extends JFrame implements WindowListener{
+public class MemberJoin extends JFrame implements WindowListener, ActionListener, KeyListener{
 	JLabel la_title; //회원가입 제목 
 	JPanel p_content; // 가운데 영역 
 	JLabel la_id, la_pass, la_jumin, la_gender, la_hobby, la_file, la_zip;
@@ -36,6 +42,9 @@ public class MemberJoin extends JFrame implements WindowListener{
 	JPasswordField t_pass; //비밀번호 
 	JTextField t_jumin1, t_jumin2;//주민번호 앞,뒤자리 
 	//Checkbox를 그룹화 시켜놓아야 라디오가 등장, html 에서도  radio는 배열로 존재시켜야 함 
+	CheckboxGroup cg;
+	Checkbox man;
+	Checkbox woman;
 	
 	Checkbox[] hobby = new Checkbox[4]; //취미
 	String[] hobbyName= {"Travel","Programming","exercise", "pet protection"};
@@ -61,6 +70,10 @@ public class MemberJoin extends JFrame implements WindowListener{
 		t_pass = new JPasswordField();
 		t_jumin1 = new JTextField();
 		t_jumin2 = new JTextField();
+		cg = new CheckboxGroup(); //그룹을 생성한다.
+		man = new Checkbox("man", cg, false); //cg 그룹에 소속시킨다
+		woman = new Checkbox("woman", cg, false); //cg 그룹에 소속시킨다
+		
 		
 		for(int i=0;i<hobby.length;i++) {
 			hobby[i] = new Checkbox(hobbyName[i]);
@@ -92,6 +105,9 @@ public class MemberJoin extends JFrame implements WindowListener{
 		t_jumin1.setPreferredSize(d2);
 		t_jumin2.setPreferredSize(d2);
 		
+		//성별 스타일 
+		la_gender.setPreferredSize(d);
+		
 		//취미 라벨 스타일
 		la_hobby.setPreferredSize(d);
 		
@@ -122,6 +138,10 @@ public class MemberJoin extends JFrame implements WindowListener{
 		p_content.add(t_jumin1);
 		p_content.add(t_jumin2);
 		
+		p_content.add(la_gender);
+		p_content.add(man);
+		p_content.add(woman);
+		
 		//취미를 반복문으로 부착 
 		p_content.add(la_hobby);
 		for(int i=0; i<hobby.length;i++) {
@@ -147,12 +167,63 @@ public class MemberJoin extends JFrame implements WindowListener{
 		//this 의 정의? 레퍼런스 변수이다.
 		this.addWindowListener(this);
 		
+		//버튼과 리스너 연결 
+		bt_regist.addActionListener(this);
+		
+		//주민번호 뒷자리 텍스트필드와 리스너 연결
+		t_jumin2.addKeyListener(this);
+		
 		//윈도우 설정 
 		setSize(700,500);
 		setVisible(true);
 	}
 	
+	//유효성 체크 
+	public void checkForm() {
+		//getText() 메서드의 반환값은  String 이고, 
+		//String 은 객체이므로 String 객체의 api를 활용할 수 있다
+		if(t_id.getText().length() <1) {
+			JOptionPane.showMessageDialog(this , "아이디를 입력하세요");
+			t_id.requestFocus();
+			return;
+		}
+		
+		//비밀번호 유효성 체크 
+		char[] pass = t_pass.getPassword();
+		if(pass.length <1) {
+			JOptionPane.showMessageDialog(this , "비밀번호를 입력하세요");
+			t_pass.requestFocus();
+			return;
+		}
+		
+		
+		//웹 : 서버로 전송하거나,
+		//독립실행형: 오라클에 직접 insert 수행
+		System.out.println("오라클에 등록할 예정");
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		//System.out.println("눌렀어?");//유효성
+		checkForm();
+	}
 	
+	//t_jumin2에 대해 키보드 눌렀다 뗄때 호출
+	public void keyReleased(KeyEvent e) {
+		char n=t_jumin2.getText().charAt(0); //주민번호 뒷자리 String 중 앞 첫 문자 반환
+		//'1'  --> 정수 1  js:  parseInt() 
+		//자바에서는 모든 기본 자료형을 객체자료형으로 바꾼다거나, 역으로 객체 자료형을 기본자료형으로 
+		//변환하는 기능을 지원하는 객체가 있으며, 이 객체를 가리켜 wrapper 클래스라 한다
+		// byte - Byte, short-Short, int-Integer ,long-Long ,float, double , char-Character, boolean
+		String s = Character.toString(n); //  '1' 을 "1"로 변환
+		int result = Integer.parseInt(s);//  "1" 을 정수 1로 변환
+		
+ 		if(result==1) { //남자라면
+			man.   //체크되어있게
+		}else if(result==2){//여자라면
+			//체크되어있게..
+		}
+ 		
+	}
 	
 	public static void main(String[] args) {
 		MemberJoin mj = new MemberJoin();
@@ -212,6 +283,20 @@ public class MemberJoin extends JFrame implements WindowListener{
 		// TODO Auto-generated method stub
 		
 	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
 }
 
 
