470e756d505d570b43c43380e6c567e51fb4e2bc
multicampus202402 <multicam202402@gmail.com>
2024-03-06 14:33:30 +0900

    금일수업

--- project0305/src/test/PaintApp.java
+++ project0305/src/test/PaintApp.java
@@ -4,11 +4,12 @@ import java.awt.Canvas;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 
-public class PaintApp extends JFrame implements MouseListener{
+public class PaintApp extends JFrame implements MouseListener, MouseMotionListener{
 	
 	Canvas can;
 	ArrayList list = new ArrayList();
@@ -16,27 +17,32 @@ public class PaintApp extends JFrame implements MouseListener{
 	public PaintApp() {
 		can = new Canvas() {
 			public void paint(Graphics g) {
-				
+				for(int i=0;i<list.size();i++) {
+					int[] pos=(int[])list.get(i);
+					
+					g.fillOval(pos[0], pos[1], 4, 4);
+				}
 			}
 		};
+		
+		add(can);
+		
+		can.addMouseListener(this);
+		can.addMouseMotionListener(this);
+		
+		setSize(800,700);
+		setVisible(true);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 	
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		int x = e.getX();
-		int y = e.getY();
-		
-		int[] dot = {x, y};
-		
-		list.add(dot);
-		
 	}
 
 	@Override
@@ -59,5 +65,28 @@ public class PaintApp extends JFrame implements MouseListener{
 	
 	public static void main(String[] args) {
 		new PaintApp();
+	}
+
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		int x = e.getX();
+		int y = e.getY();
+		
+		int[] dot = {x, y};
+		
+		list.add(dot);
+		
+		can.repaint();
+		System.out.println(dot[0]+","+dot[1]);
+
+		
+	}
+
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
 	}
 }
