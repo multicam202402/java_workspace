3911e4bdb317cb8f36a12140756e6da310984791
multicampus202402 <multicam202402@gmail.com>
2024-03-06 15:18:00 +0900

    금일수업

--- project0306/src/com/sds/project0306/gallery/GalleryMain.java
+++ project0306/src/com/sds/project0306/gallery/GalleryMain.java
@@ -2,12 +2,14 @@ package com.sds.project0306.gallery;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
-public class GalleryMain extends JFrame{
+public class GalleryMain extends JFrame implements ActionListener{
 	MyCanvas can;
 	JPanel p_south;
 	JButton bt_prev, bt_next;
@@ -26,10 +28,18 @@ public class GalleryMain extends JFrame{
 		p_south.add(bt_prev); //패널에 버튼 부착
 		p_south.add(bt_next); //패널에 버튼 부착
 		add(p_south, BorderLayout.SOUTH);//남쪽에 패널 부착 
-			
+		
+		//버튼과 리스너 연결 
+		bt_prev.addActionListener(this);
+		bt_next.addActionListener(this);
+		
 		setSize(600,600);
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		System.out.println(e);
 	}
 	
 	public static void main(String[] args) {
