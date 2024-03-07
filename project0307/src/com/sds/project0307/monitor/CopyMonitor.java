5b7c1a83821598608ee2ffb8e2068965d9f49031
multicampus202402 <multicam202402@gmail.com>
2024-03-07 14:28:06 +0900

    금일수업

--- project0307/src/com/sds/project0307/monitor/CopyMonitor.java
+++ project0307/src/com/sds/project0307/monitor/CopyMonitor.java
@@ -2,28 +2,39 @@ package com.sds.project0307.monitor;
 
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 //파일을 복사하는 과정에서 실행중인 프로그램으로 들어오고 나가는 바이트들을 육안으로 확인해보기 위한
 //프로그램 
-public class CopyMonitor extends JFrame{
+public class CopyMonitor extends JFrame implements ActionListener, WindowListener{
 	JLabel la_ori, la_dest;
 	JTextField t_ori, t_dest;
 	JTextArea area;
 	JButton bt;
+	FileInputStream fis;
+	FileOutputStream fos;
 	
 	public CopyMonitor() {
 		super("복사 데이터 모니터링"); //부모님인 JFrame의 생성자 호출
 		
 		la_ori = new JLabel("원본 경로");
 		la_dest = new JLabel("복사 경로");
-		t_ori = new JTextField("");
-		t_dest = new JTextField("");
+		t_ori = new JTextField("D:/java_workspace/project0307/res/winter_night.jpg");
+		t_dest = new JTextField("D:/java_workspace/project0307/res/winter_night3.jpg");
 		area = new JTextArea();
 		bt = new JButton("복사실행");
 		
@@ -48,14 +59,105 @@ public class CopyMonitor extends JFrame{
 		add(area);
 		add(bt);
 		
+		bt.addActionListener(this); //버튼과 리스너 연결 
+		this.addWindowListener(this);//윈도우창과 리스너 연결 
+		
 		//윈도우 설정 
 		setSize(800,600);
 		setVisible(true);
 		setLocationRelativeTo(null);
 	}
 	
+	//스트림을 이용하여, 데이터 읽고, 내뱉기
+	public void copy() {
+		//스트림 생성하기 
+		try {
+			fis = new FileInputStream(t_ori.getText()); //파일을 대상으로 한 입력 스트림
+			fos = new FileOutputStream(t_dest.getText()); //파일을 대상으로 한 출력 스트림
+			this.setTitle("파일을 대상으로 스트림 생성함");
+			
+			//파일의 끝에 도달할때까지, 읽기와 쓰기를 반복해보자 
+			int data=-1;
+			
+			while(true) {
+				data=fis.read(); //1 byte 읽기
+				if(data==-1)break;//루프 중단
+				fos.write(data); //1 byte 쓰기
+			}
+			JOptionPane.showMessageDialog(this, "복사완료");
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			this.setTitle("스트림 생성 실패");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		copy();
+	}
+	
+	public void windowClosing(WindowEvent e) {
+		//스트림이 생성되어 있다면 닫자 
+		if(fis !=null) {
+			try {
+				fis.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		
+		if(fos !=null) {
+			try {
+				fos.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}	
+	
 	public static void main(String[] args) {
 		new CopyMonitor();
+	}
+
+	@Override
+	public void windowOpened(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void windowClosed(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowActivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
