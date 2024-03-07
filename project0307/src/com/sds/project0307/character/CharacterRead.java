6d88c2c2a31529c6a4275ba956e83c6a6a6a52c7
multicampus202402 <multicam202402@gmail.com>
2024-03-07 16:12:59 +0900

    금일수업

--- project0307/src/com/sds/project0307/character/CharacterRead.java
+++ project0307/src/com/sds/project0307/character/CharacterRead.java
@@ -1,5 +1,6 @@
 package com.sds.project0307.character;
 
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -24,19 +25,33 @@ public class CharacterRead {
 								//데이터가 영미권 이외의
 								//문자라 할지라도, 2byte를 하나로 묶어서 이해할 수 있는 기능이 지원되므로 
 								//영미권 이외의 문자가 깨지지 않고 처리될 수 있다.
+	
+	//리더 스트림은 문자를 1자씩 읽어들이므로, 문서파일의 용량이 크면, 효율성이 매우 떨어질 수 있다..
+	//따라서 버퍼를 이용하여, 문자를 모아서 즉 문자열로 모은 후 읽어들이는 방법을 이용한다면 
+	//read()  횟수 즉 읽어들이는 횟수를 획기적으로 줄일 수 있다..
+	//자바에서는 버퍼처리된 스트림은 접두어가 Buffered~~ 가 붙는다 
+	BufferedReader buffr; //문자 기반 스트림을 대상으로 버퍼기능을 지원하는 스트림
+	
 	public CharacterRead() {
 		
 		try {
+			
 			reader = new FileReader("D:/java_workspace/project0307/res/memo.txt");
 			
+			//기존의 문자기반 스트림인 reader 를 버퍼기반으로 업그레이드(빨대를 덧 씌움)
+			buffr = new BufferedReader(reader);
+			
 			//데이터를 읽어와 보자
-			int data=-1;
+			String data=null; //String 객체이므로, 아무것도 읽어들이지 않앗다면 null
 			
 			//read() 메서드 몇번 수행할지를 테스트해본다
+			int count=0; //read() 횟수 알아보려고..
+			
 			while(true) {
-				data = reader.read(); //1바이트가 아닌 1문자를 읽어들인다(영문이건, 한글이건..)
-				if(data==-1)break;
-				System.out.print((char)data);
+				data = buffr.readLine();
+				count++;
+				if(data==null)break;
+				System.out.println(data+", count="+count);
 			}
 			
 		} catch (FileNotFoundException e) {
