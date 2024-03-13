a1fc00c5141e240ef6f0c70346069ee547ab320c
multicampus202402 <multicam202402@gmail.com>
2024-03-13 13:21:49 +0900

    금일수업

--- SeShop/src/com/sds/seshop/admin/AdminRegist.java
+++ SeShop/src/com/sds/seshop/admin/AdminRegist.java
@@ -18,6 +18,7 @@ public class AdminRegist extends Page{
 	JPasswordField t_pass;
 	JTextField t_email;
 	JButton bt_profile;//프로필 사진 선택 창 띄우기 버튼
+	JLabel la_dummy;//프로필 사진의 공백을 메울 목적
 	JPanel p_preview; //사진 미리보기 
 	JButton bt_regist, bt_login;
 	
@@ -32,6 +33,7 @@ public class AdminRegist extends Page{
 		la_pass = new JLabel("Password");
 		la_email = new JLabel("Email");
 		la_profile = new JLabel("프로필사진");
+		la_dummy=new JLabel("");
 		
 		t_id  = new JTextField();
 		t_pass  =new JPasswordField();
@@ -55,9 +57,25 @@ public class AdminRegist extends Page{
 		t_email.setPreferredSize(d);
 		la_profile.setPreferredSize(d);
 		bt_profile.setPreferredSize(d);
+		la_dummy.setPreferredSize(d);
 		
-		p_preview.setPreferredSize(new Dimension(400, 400));
+		p_preview.setPreferredSize(new Dimension(280, 280));
 		
+		//조립 
+		container.add(la_id);
+		container.add(t_id);
+		container.add(la_pass);
+		container.add(t_pass);
+		container.add(la_email);
+		container.add(t_email);
+		container.add(la_profile);
+		container.add(bt_profile);
+		container.add(la_dummy);
+		container.add(p_preview);
+		container.add(bt_regist);
+		container.add(bt_login);	
+		
+		add(container);
 	}
 }
 
