1c869a111796628a8030baf4f6706b4244a4fe08
multicampus202402 <multicam202402@gmail.com>
2024-03-15 11:58:40 +0900

    금일수업

--- SeShop/src/com/sds/seshop/product/ProductList.java
+++ SeShop/src/com/sds/seshop/product/ProductList.java
@@ -3,12 +3,16 @@ package com.sds.seshop.product;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
@@ -21,7 +25,11 @@ public class ProductList extends Page{
 	public JTable table;
 	JScrollPane scroll;
 	JPanel p_south;
+	JPanel p_preview; //상세 상품 이미지 그려질 패널
+	JLabel la_product_name, la_price, la_brand;
+	
 	ProductTableModel model;
+	Image image; //최초 null
 	
 	public ProductList(ShopMain shopMain) {
 		super(Color.RED);
@@ -38,12 +46,34 @@ public class ProductList extends Page{
 		table = new JTable(model = new ProductTableModel());
 		scroll =new JScrollPane(table);
 		p_south = new JPanel();
+		p_preview = new JPanel() {
+			public void paint(Graphics g) {
+				g.setColor(Color.YELLOW);//페인트 통 색상 지정
+				g.fillRect(0, 0, 350, 280);
+				
+				g.drawImage(image, 0, 0, 350, 280, p_preview);
+			}
+		};
+		
+		la_product_name = new JLabel("상품명 나올곳"); //상품명 나올 곳
+		la_price = new JLabel("가격 나올곳"); //가격 나올 곳
+		la_brand = new JLabel("브랜드 나올곳"); //브랜드명 나올 곳
 		
 		//스타일 
 		p_south.setPreferredSize(new Dimension(800, 250));
 		p_south.setBackground(Color.CYAN);
+		p_preview.setPreferredSize(new Dimension(350, 280));
+		Dimension d = new Dimension(320,50);
+		la_product_name.setPreferredSize(d);
+		la_price.setPreferredSize(d);
+		la_brand.setPreferredSize(d);
 		
 		//조립 
+		p_south.add(p_preview);
+		p_south.add(la_product_name);
+		p_south.add(la_price);
+		p_south.add(la_brand);
+		
 		container.add(scroll); //센터에 부착됨 
 		container.add(p_south, BorderLayout.SOUTH); //south 에 부착
 		add(container);
@@ -67,8 +97,44 @@ public class ProductList extends Page{
 	
 	//선택한 상품 한건 가져오기 
 	public void getProductDetail(int product_idx) {
+		PreparedStatement pstmt=null;
+		ResultSet rs=null;
+		
 		String sql="select * from product where product_idx="+product_idx;
 		System.out.println(sql);
+		
+		try {
+			pstmt=shopMain.con.prepareStatement(sql);
+			rs = pstmt.executeQuery(); //select 문 수행 후 결과 반환
+			
+			//rs 의 값을 상세보기 패널에 반영하자!
+			rs.next(); //커서 한칸 전진
+			String filename = "C:/Users/zino1/SeShop/"+rs.getString("filename");
+			ImageIcon icon =new ImageIcon(filename);
+			image = icon.getImage(); //아이콘을 이미지로 변환
+			p_preview.repaint();//패널에게 그림을 다시 그리도록 요청 
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	
+		
 	}
 	
 	//상품 가져오기 
@@ -102,6 +168,22 @@ public class ProductList extends Page{
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}finally {
+			if(rs!=null) {
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(pstmt!=null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			
 		}
 		
 	}
